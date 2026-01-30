45fc8607fa6274cfc7351a7c5f721c388a80d827
phamLinh98 <linhthusinh98@gmail.com>
2026-01-30 23:50:57 +0900

    Reorganize import statements in ListDAL for better readability

--- src/main/java/com/example/java_backend/DAL/ListDAL.java
+++ src/main/java/com/example/java_backend/DAL/ListDAL.java
@@ -1,16 +1,16 @@
 package com.example.java_backend.DAL;
 
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Repository
 public class ListDAL {
