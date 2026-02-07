efc857697e31806698cd2dd21cee605294bd0ca2
phamLinh98 <linhthusinh98@gmail.com>
2026-02-08 02:17:00 +0900

    refact xml file and Config.java

--- src/main/java/com/example/java_backend/config/MyBatisConfig.java
+++ src/main/java/com/example/java_backend/config/MyBatisConfig.java
@@ -4,13 +4,11 @@ import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
-@MapperScan("com.example.java_backend.mappers")
 public class MyBatisConfig {
 
         @Bean
