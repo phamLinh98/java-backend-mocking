196d1ea873a1f35391a477a8719f02dbb0f44650
phamLinh98 <linhthusinh98@gmail.com>
2026-02-08 02:20:17 +0900

    add anotivation to Main

--- src/main/java/com/example/java_backend/JavaBackendApplication.java
+++ src/main/java/com/example/java_backend/JavaBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.java_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.java_backend.mappers")
 public class JavaBackendApplication {
 
 	public static void main(String[] args) {
